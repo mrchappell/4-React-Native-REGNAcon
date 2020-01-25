8c31060a0651f0d9d927464678936f72b8912b97
mrchappell <michael.r.chappell@gmail.com>
2020-01-25 01:25:40 -0600

    Activity monitor

--- REGNAcon/components/GuestsComponent.js
+++ REGNAcon/components/GuestsComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -31,8 +32,18 @@ class Guests extends Component {
             );
         };
 
+        if (this.props.celebs.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.celebs.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.celebs.errMess}</Text>
+               </View>
+            );
+        }
         return (
-            <FlatList
+            <FlatList  
                 data={this.props.celebs.celebs}
                 renderItem={renderGuestItem}
                 keyExtractor={item => item.id.toString()}
--- REGNAcon/components/HomeComponent.js
+++ REGNAcon/components/HomeComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -15,6 +16,16 @@ const mapStateToProps = state => {
 function RenderItem(props) {
     const {item} = props;
 
+    if (props.isLoading) {
+        return <Loading />;
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
     if (item) {
         return (
             <Card
@@ -41,12 +52,18 @@ class Home extends Component {
             <ScrollView>
                 <RenderItem
                     item={this.props.celebs.celebs.filter(celeb => celeb.featured)[0]}
+                    isLoading={this.props.celebs.isLoading}
+                    errMess={this.props.celebs.errMess}
                 />
                 <RenderItem
                     item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess} 
                 />
                 <RenderItem
                     item={this.props.partners.partners.filter(partner => partner.featured)[0]}
+                    isLoading={this.props.partners.isLoading}
+                    errMess={this.props.partners.errMess} 
                 />
             </ScrollView>
         );
